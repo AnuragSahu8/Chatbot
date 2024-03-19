a9084d2413660862ef7aa5d53fef2ff4cf46453a
Anurag Sahu <sahuanuragofficial@gmail.com>
2024-03-19 15:48:55 +0530

    update main.py

--- backend/main.py
+++ backend/main.py
@@ -146,5 +146,5 @@ app.add_middleware(
   
 #   return {"result": "hello world!"}
 
-if __name__=="__main__":
-    uvicorn.run(app,host="localhost",port=8000)
+# if __name__=="__main__":
+#     uvicorn.run(app,host="localhost",port=8000)
